67d56ae028173f4469f9dbecd46d11cdabe1562d
maxifly <maxpant@mail.ru>
2016-05-27 00:32:35 +0300

    fix

--- src/main/java/com/kuku/rest/model/LatestVersion.java
+++ src/main/java/com/kuku/rest/model/LatestVersion.java
@@ -1,10 +1,14 @@
 package com.kuku.rest.model;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Maximus on 26.05.2016.
  */
 public class LatestVersion {
     public String url;
     public String tag_name;
-    public Asset[] assets;
+    public List<Asset> assets = new ArrayList<>();
 }
